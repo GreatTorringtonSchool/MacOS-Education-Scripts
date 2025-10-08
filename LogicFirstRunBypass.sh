0c4473c9224709cd48c446d124f542da0c1f2fe7
Great Torrington School <ictsupport@gts.devon.sch.uk>
2025-10-08 10:37:00 +0100

    Update LogicFirstRunBypass.sh
    Add comments detailing what the second command does

--- LogicFirstRunBypass.sh
+++ LogicFirstRunBypass.sh
@@ -1,6 +1,7 @@
 #Convince Logic that it has been launched before as the current user
 defaults write ~/Library/Preferences/com.apple.logic10 MANPDNotFirstLaunch -bool true
 
+#Bypass the What's New screen by setting the integer to a higher integer than what is available in the Logic App
 #Update the integer whenever the What's New panel goes beyond 24, as of October 2025 it is at 17
 defaults write ~/Library/Preferences/com.apple.logic10 RecentWhatsNewPanelVersion -int 24
 
