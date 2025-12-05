9d5bb30529eb45fb54ecdbe9cb041a774afc4edb
Great Torrington School <ictsupport@gts.devon.sch.uk>
2025-12-05 14:04:10 +0000

    Set default MP3 Bounce settings in Logic preferences
    Add default Bounce settings for MP3 format in Logic.

--- LogiProDefaultSettings.sh
+++ LogiProDefaultSettings.sh
@@ -10,5 +10,16 @@ defaults write ~/Library/Preferences/com.apple.logic10 DontShowAdditionalContent
 #Update the integer whenever the What's New panel goes beyond 24, as of October 2025 it is at 17
 defaults write ~/Library/Preferences/com.apple.logic10 RecentWhatsNewPanelVersion -int 24
 
+##Set default Bounce settings
+#Use MP3 format
+defaults write ~/Library/Preferences/com.apple.logic10 BounceFileType -int 2
+#Set our MP3 settings
+defaults write ~/Library/Preferences/com.apple.logic10 "MP3Prefs.BitRateMono" -int 160
+defaults write ~/Library/Preferences/com.apple.logic10 "MP3Prefs.BitRateStereo" -int 320
+defaults write ~/Library/Preferences/com.apple.logic10 "MP3Prefs.FilterSub10Hz" -bool true
+defaults write ~/Library/Preferences/com.apple.logic10 "MP3Prefs.JointStereo" -bool true
+defaults write ~/Library/Preferences/com.apple.logic10 "MP3Prefs.VBRLevel" -int 6
+defaults write ~/Library/Preferences/com.apple.logic10 "MP3Prefs.VariableBitRate" -bool false
+
 #Refresh the preference cash by killing the daemon
 killall cfprefsd
